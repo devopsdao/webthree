cb820c1858ef28be3c3dd1bcf18328cf7239e14f
Mahdi <mkfard@outlook.com>
2022-02-16 20:30:47 +0330

    Erc20 class support for `name`, `symbol` and `decimals` functions.

--- lib/src/generated/erc20.g.dart
+++ lib/src/generated/erc20.g.dart
@@ -3,7 +3,7 @@
 import 'package:web3dart/web3dart.dart' as _i1;
 
 final _contractAbi = _i1.ContractAbi.fromJson(
-    '[{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}]',
+    '[{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}]',
     'Erc20');
 
 /// Interface of the ERC20 standard as defined in the EIP.
@@ -57,13 +57,52 @@ class Erc20 extends _i1.GeneratedContract {
     return (response[0] as BigInt);
   }
 
+  /// Returns the decimal precision of the token.
+  ///
+  /// The optional [atBlock] parameter can be used to view historical data. When
+  /// set, the function will be evaluated in the specified block. By default, the
+  /// latest on-chain block will be used.
+  Future<BigInt> decimals({_i1.BlockNum? atBlock}) async {
+    final function = self.abi.functions[3];
+    assert(checkSignature(function, '313ce567'));
+    final params = [];
+    final response = await read(function, params, atBlock);
+    return (response[0] as BigInt);
+  }
+
+  /// Returns the name of the token.
+  ///
+  /// The optional [atBlock] parameter can be used to view historical data. When
+  /// set, the function will be evaluated in the specified block. By default, the
+  /// latest on-chain block will be used.
+  Future<String> name({_i1.BlockNum? atBlock}) async {
+    final function = self.abi.functions[4];
+    assert(checkSignature(function, '06fdde03'));
+    final params = [];
+    final response = await read(function, params, atBlock);
+    return (response[0] as String);
+  }
+
+  /// Returns the symbol of the token.
+  ///
+  /// The optional [atBlock] parameter can be used to view historical data. When
+  /// set, the function will be evaluated in the specified block. By default, the
+  /// latest on-chain block will be used.
+  Future<String> symbol({_i1.BlockNum? atBlock}) async {
+    final function = self.abi.functions[5];
+    assert(checkSignature(function, '95d89b41'));
+    final params = [];
+    final response = await read(function, params, atBlock);
+    return (response[0] as String);
+  }
+
   /// Returns the amount of tokens in existence.
   ///
   /// The optional [atBlock] parameter can be used to view historical data. When
   /// set, the function will be evaluated in the specified block. By default, the
   /// latest on-chain block will be used.
   Future<BigInt> totalSupply({_i1.BlockNum? atBlock}) async {
-    final function = self.abi.functions[3];
+    final function = self.abi.functions[6];
     assert(checkSignature(function, '18160ddd'));
     final params = [];
     final response = await read(function, params, atBlock);
@@ -78,7 +117,7 @@ class Erc20 extends _i1.GeneratedContract {
   Future<String> transfer(_i1.EthereumAddress recipient, BigInt amount,
       {required _i1.Credentials credentials,
       _i1.Transaction? transaction}) async {
-    final function = self.abi.functions[4];
+    final function = self.abi.functions[7];
     assert(checkSignature(function, 'a9059cbb'));
     final params = [recipient, amount];
     return write(credentials, transaction, function, params);
@@ -93,7 +132,7 @@ class Erc20 extends _i1.GeneratedContract {
       _i1.EthereumAddress sender, _i1.EthereumAddress recipient, BigInt amount,
       {required _i1.Credentials credentials,
       _i1.Transaction? transaction}) async {
-    final function = self.abi.functions[5];
+    final function = self.abi.functions[8];
     assert(checkSignature(function, '23b872dd'));
     final params = [sender, recipient, amount];
     return write(credentials, transaction, function, params);
