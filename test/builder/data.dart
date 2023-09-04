67b5c697b9879fba61288773476d94fd3bf631d2
user1 <user1@localhost>
2023-09-04 13:02:26 +0300

    fixed tests

--- test/builder/data.dart
+++ test/builder/data.dart
@@ -38,28 +38,49 @@ const testCases = <String, String>{
 ]''': r'''
 // Generated code, do not modify. Run `build_runner build` to re-generate!
 // @dart=2.12
+// ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:webthree/webthree.dart' as _i1;
 
 final _contractAbi = _i1.ContractAbi.fromJson(
-    '[{"inputs":[{"internalType":"uint240","name":"first","type":"uint240"},{"internalType":"uint248","name":"second","type":"uint248"}],"name":"retrieve3","outputs":[{"internalType":"string","name":"","type":"string"},{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"pure","type":"function"}]',
-    'Contract');
+  '[{"inputs":[{"internalType":"uint240","name":"first","type":"uint240"},{"internalType":"uint248","name":"second","type":"uint248"}],"name":"retrieve3","outputs":[{"internalType":"string","name":"","type":"string"},{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"pure","type":"function"}]',
+  'Contract',
+);
 
 class Contract extends _i1.GeneratedContract {
-  Contract(
-      {required _i1.EthereumAddress address,
-      required _i1.Web3Client client,
-      int? chainId})
-      : super(_i1.DeployedContract(_contractAbi, address), client, chainId);
+  Contract({
+    required _i1.EthereumAddress address,
+    required _i1.Web3Client client,
+    int? chainId,
+  }) : super(
+          _i1.DeployedContract(
+            _contractAbi,
+            address,
+          ),
+          client,
+          chainId,
+        );
 
   /// The optional [atBlock] parameter can be used to view historical data. When
   /// set, the function will be evaluated in the specified block. By default, the
   /// latest on-chain block will be used.
-  Future<Retrieve3> retrieve3(BigInt first, BigInt second,
-      {_i1.BlockNum? atBlock}) async {
+  Future<Retrieve3> retrieve3(
+    BigInt first,
+    BigInt second, {
+    _i1.BlockNum? atBlock,
+    _i1.EthereumAddress? sender,
+  }) async {
     final function = self.abi.functions[0];
     assert(checkSignature(function, '2783b284'));
-    final params = [first, second];
-    final response = await read(function, params, atBlock);
+    final params = [
+      first,
+      second,
+    ];
+    final response = await read(
+      sender,
+      function,
+      params,
+      atBlock,
+    );
     return Retrieve3(response);
   }
 }
@@ -131,51 +152,83 @@ class Retrieve3 {
 ]''': r'''
 // Generated code, do not modify. Run `build_runner build` to re-generate!
 // @dart=2.12
+// ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:webthree/webthree.dart' as _i1;
 
 final _contractAbi = _i1.ContractAbi.fromJson(
-    '[{"inputs":[],"name":"giveMeHello","outputs":[{"internalType":"string","name":"message","type":"string"},{"internalType":"uint256","name":"num1","type":"uint256"},{"internalType":"uint256","name":"num2","type":"uint256"}],"stateMutability":"pure","type":"function"},{"inputs":[],"name":"retrieve","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"num","type":"uint256"}],"name":"store","outputs":[],"stateMutability":"nonpayable","type":"function"}]',
-    'Contract');
+  '[{"inputs":[],"name":"giveMeHello","outputs":[{"internalType":"string","name":"message","type":"string"},{"internalType":"uint256","name":"num1","type":"uint256"},{"internalType":"uint256","name":"num2","type":"uint256"}],"stateMutability":"pure","type":"function"},{"inputs":[],"name":"retrieve","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"num","type":"uint256"}],"name":"store","outputs":[],"stateMutability":"nonpayable","type":"function"}]',
+  'Contract',
+);
 
 class Contract extends _i1.GeneratedContract {
-  Contract(
-      {required _i1.EthereumAddress address,
-      required _i1.Web3Client client,
-      int? chainId})
-      : super(_i1.DeployedContract(_contractAbi, address), client, chainId);
+  Contract({
+    required _i1.EthereumAddress address,
+    required _i1.Web3Client client,
+    int? chainId,
+  }) : super(
+          _i1.DeployedContract(
+            _contractAbi,
+            address,
+          ),
+          client,
+          chainId,
+        );
 
   /// The optional [atBlock] parameter can be used to view historical data. When
   /// set, the function will be evaluated in the specified block. By default, the
   /// latest on-chain block will be used.
-  Future<GiveMeHello> giveMeHello({_i1.BlockNum? atBlock}) async {
+  Future<GiveMeHello> giveMeHello({
+    _i1.BlockNum? atBlock,
+    _i1.EthereumAddress? sender,
+  }) async {
     final function = self.abi.functions[0];
     assert(checkSignature(function, '1c1657ea'));
     final params = [];
-    final response = await read(function, params, atBlock);
+    final response = await read(
+      sender,
+      function,
+      params,
+      atBlock,
+    );
     return GiveMeHello(response);
   }
 
   /// The optional [atBlock] parameter can be used to view historical data. When
   /// set, the function will be evaluated in the specified block. By default, the
   /// latest on-chain block will be used.
-  Future<BigInt> retrieve({_i1.BlockNum? atBlock}) async {
+  Future<BigInt> retrieve({
+    _i1.BlockNum? atBlock,
+    _i1.EthereumAddress? sender,
+  }) async {
     final function = self.abi.functions[1];
     assert(checkSignature(function, '2e64cec1'));
     final params = [];
-    final response = await read(function, params, atBlock);
+    final response = await read(
+      sender,
+      function,
+      params,
+      atBlock,
+    );
     return (response[0] as BigInt);
   }
 
   /// The optional [transaction] parameter can be used to override parameters
   /// like the gas price, nonce and max gas. The `data` and `to` fields will be
   /// set by the contract.
-  Future<String> store(BigInt num,
-      {required _i1.Credentials credentials,
-      _i1.Transaction? transaction}) async {
+  Future<String> store(
+    BigInt num, {
+    required _i1.Credentials credentials,
+    _i1.Transaction? transaction,
+  }) async {
     final function = self.abi.functions[2];
     assert(checkSignature(function, '6057361d'));
     final params = [num];
-    return write(credentials, transaction, function, params);
+    return write(
+      credentials,
+      transaction,
+      function,
+      params,
+    );
   }
 }
 
@@ -197,17 +250,17 @@ class GiveMeHello {
     {
         "inputs": [
             {
-                "internalType": "address",
-                "name": "",
-                "type": "address"
+  "internalType": "address",
+  "name": "",
+  "type": "address"
             }
         ],
         "name": "userTotalAmount",
         "outputs": [
             {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
+  "internalType": "uint256",
+  "name": "",
+  "type": "uint256"
             }
         ],
         "stateMutability": "view",
@@ -216,28 +269,45 @@ class GiveMeHello {
 ]''': r'''
 // Generated code, do not modify. Run `build_runner build` to re-generate!
 // @dart=2.12
+// ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:webthree/webthree.dart' as _i1;
 
 final _contractAbi = _i1.ContractAbi.fromJson(
-    '[{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"userTotalAmount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]',
-    'Contract');
+  '[{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"userTotalAmount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]',
+  'Contract',
+);
 
 class Contract extends _i1.GeneratedContract {
-  Contract(
-      {required _i1.EthereumAddress address,
-      required _i1.Web3Client client,
-      int? chainId})
-      : super(_i1.DeployedContract(_contractAbi, address), client, chainId);
+  Contract({
+    required _i1.EthereumAddress address,
+    required _i1.Web3Client client,
+    int? chainId,
+  }) : super(
+          _i1.DeployedContract(
+            _contractAbi,
+            address,
+          ),
+          client,
+          chainId,
+        );
 
   /// The optional [atBlock] parameter can be used to view historical data. When
   /// set, the function will be evaluated in the specified block. By default, the
   /// latest on-chain block will be used.
-  Future<BigInt> userTotalAmount(_i1.EthereumAddress $param0,
-      {_i1.BlockNum? atBlock}) async {
+  Future<BigInt> userTotalAmount(
+    _i1.EthereumAddress $param0, {
+    _i1.BlockNum? atBlock,
+    _i1.EthereumAddress? sender,
+  }) async {
     final function = self.abi.functions[0];
     assert(checkSignature(function, '7dbfb5dc'));
     final params = [$param0];
-    final response = await read(function, params, atBlock);
+    final response = await read(
+      sender,
+      function,
+      params,
+      atBlock,
+    );
     return (response[0] as BigInt);
   }
 }
@@ -256,18 +326,27 @@ class Contract extends _i1.GeneratedContract {
 ]''': r'''
 // Generated code, do not modify. Run `build_runner build` to re-generate!
 // @dart=2.12
+// ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:webthree/webthree.dart' as _i1;
 
 final _contractAbi = _i1.ContractAbi.fromJson(
-    '[{"payable":true,"stateMutability":"payable","type":"fallback"},{"stateMutability":"payable","type":"receive"}]',
-    'Contract');
+  '[{"payable":true,"stateMutability":"payable","type":"fallback"},{"stateMutability":"payable","type":"receive"}]',
+  'Contract',
+);
 
 class Contract extends _i1.GeneratedContract {
-  Contract(
-      {required _i1.EthereumAddress address,
-      required _i1.Web3Client client,
-      int? chainId})
-      : super(_i1.DeployedContract(_contractAbi, address), client, chainId);
+  Contract({
+    required _i1.EthereumAddress address,
+    required _i1.Web3Client client,
+    int? chainId,
+  }) : super(
+          _i1.DeployedContract(
+            _contractAbi,
+            address,
+          ),
+          client,
+          chainId,
+        );
 }
 ''',
   r'''
@@ -277,9 +356,9 @@ class Contract extends _i1.GeneratedContract {
         "name": "test",
         "outputs": [
             {
-                "internalType": "string[][6][3]",
-                "name": "",
-                "type": "string[][6][3]"
+  "internalType": "string[][6][3]",
+  "name": "",
+  "type": "string[][6][3]"
             }
         ],
         "stateMutability": "view",
@@ -288,27 +367,44 @@ class Contract extends _i1.GeneratedContract {
 ]''': r'''
 // Generated code, do not modify. Run `build_runner build` to re-generate!
 // @dart=2.12
+// ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:webthree/webthree.dart' as _i1;
 
 final _contractAbi = _i1.ContractAbi.fromJson(
-    '[{"inputs":[],"name":"test","outputs":[{"internalType":"string[][6][3]","name":"","type":"string[][6][3]"}],"stateMutability":"view","type":"function"}]',
-    'Contract');
+  '[{"inputs":[],"name":"test","outputs":[{"internalType":"string[][6][3]","name":"","type":"string[][6][3]"}],"stateMutability":"view","type":"function"}]',
+  'Contract',
+);
 
 class Contract extends _i1.GeneratedContract {
-  Contract(
-      {required _i1.EthereumAddress address,
-      required _i1.Web3Client client,
-      int? chainId})
-      : super(_i1.DeployedContract(_contractAbi, address), client, chainId);
+  Contract({
+    required _i1.EthereumAddress address,
+    required _i1.Web3Client client,
+    int? chainId,
+  }) : super(
+          _i1.DeployedContract(
+            _contractAbi,
+            address,
+          ),
+          client,
+          chainId,
+        );
 
   /// The optional [atBlock] parameter can be used to view historical data. When
   /// set, the function will be evaluated in the specified block. By default, the
   /// latest on-chain block will be used.
-  Future<List<List<List<String>>>> test({_i1.BlockNum? atBlock}) async {
+  Future<List<List<List<String>>>> test({
+    _i1.BlockNum? atBlock,
+    _i1.EthereumAddress? sender,
+  }) async {
     final function = self.abi.functions[0];
     assert(checkSignature(function, 'f8a8fd6d'));
     final params = [];
-    final response = await read(function, params, atBlock);
+    final response = await read(
+      sender,
+      function,
+      params,
+      atBlock,
+    );
     return (response[0] as List<dynamic>)
         .cast<List<dynamic>>()
         .map<List<List<String>>>((e) {
@@ -330,16 +426,27 @@ class Contract extends _i1.GeneratedContract {
 }''': r'''
 // Generated code, do not modify. Run `build_runner build` to re-generate!
 // @dart=2.12
+// ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:webthree/webthree.dart' as _i1;
 
-final _contractAbi = _i1.ContractAbi.fromJson('[]', 'Contract');
+final _contractAbi = _i1.ContractAbi.fromJson(
+  '[]',
+  'Contract',
+);
 
 class Contract extends _i1.GeneratedContract {
-  Contract(
-      {required _i1.EthereumAddress address,
-      required _i1.Web3Client client,
-      int? chainId})
-      : super(_i1.DeployedContract(_contractAbi, address), client, chainId);
+  Contract({
+    required _i1.EthereumAddress address,
+    required _i1.Web3Client client,
+    int? chainId,
+  }) : super(
+          _i1.DeployedContract(
+            _contractAbi,
+            address,
+          ),
+          client,
+          chainId,
+        );
 }
 ''',
   r'''
@@ -347,19 +454,19 @@ class Contract extends _i1.GeneratedContract {
     {
         "inputs": [
             {
-                "internalType": "address",
-                "name": "_from",
-                "type": "address"
+  "internalType": "address",
+  "name": "_from",
+  "type": "address"
             },
             {
-                "internalType": "address",
-                "name": "_to",
-                "type": "address"
+  "internalType": "address",
+  "name": "_to",
+  "type": "address"
             },
             {
-                "internalType": "uint256",
-                "name": "_tokenId",
-                "type": "uint256"
+  "internalType": "uint256",
+  "name": "_tokenId",
+  "type": "uint256"
             }
         ],
         "name": "safeTransferFrom",
@@ -370,24 +477,24 @@ class Contract extends _i1.GeneratedContract {
     {
         "inputs": [
             {
-                "internalType": "address",
-                "name": "_from",
-                "type": "address"
+  "internalType": "address",
+  "name": "_from",
+  "type": "address"
             },
             {
-                "internalType": "address",
-                "name": "_to",
-                "type": "address"
+  "internalType": "address",
+  "name": "_to",
+  "type": "address"
             },
             {
-                "internalType": "uint256",
-                "name": "_tokenId",
-                "type": "uint256"
+  "internalType": "uint256",
+  "name": "_tokenId",
+  "type": "uint256"
             },
             {
-                "internalType": "bytes",
-                "name": "data",
-                "type": "bytes"
+  "internalType": "bytes",
+  "name": "data",
+  "type": "bytes"
             }
         ],
         "name": "safeTransferFrom",
@@ -398,44 +505,79 @@ class Contract extends _i1.GeneratedContract {
 ]''': r'''
 // Generated code, do not modify. Run `build_runner build` to re-generate!
 // @dart=2.12
+// ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:webthree/webthree.dart' as _i1;
 import 'dart:typed_data' as _i2;
 
 final _contractAbi = _i1.ContractAbi.fromJson(
-    '[{"inputs":[{"internalType":"address","name":"_from","type":"address"},{"internalType":"address","name":"_to","type":"address"},{"internalType":"uint256","name":"_tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"address","name":"_from","type":"address"},{"internalType":"address","name":"_to","type":"address"},{"internalType":"uint256","name":"_tokenId","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"payable","type":"function"}]',
-    'Contract');
+  '[{"inputs":[{"internalType":"address","name":"_from","type":"address"},{"internalType":"address","name":"_to","type":"address"},{"internalType":"uint256","name":"_tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"address","name":"_from","type":"address"},{"internalType":"address","name":"_to","type":"address"},{"internalType":"uint256","name":"_tokenId","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"payable","type":"function"}]',
+  'Contract',
+);
 
 class Contract extends _i1.GeneratedContract {
-  Contract(
-      {required _i1.EthereumAddress address,
-      required _i1.Web3Client client,
-      int? chainId})
-      : super(_i1.DeployedContract(_contractAbi, address), client, chainId);
+  Contract({
+    required _i1.EthereumAddress address,
+    required _i1.Web3Client client,
+    int? chainId,
+  }) : super(
+          _i1.DeployedContract(
+            _contractAbi,
+            address,
+          ),
+          client,
+          chainId,
+        );
 
   /// The optional [transaction] parameter can be used to override parameters
   /// like the gas price, nonce and max gas. The `data` and `to` fields will be
   /// set by the contract.
   Future<String> safeTransferFrom(
-      _i1.EthereumAddress _from, _i1.EthereumAddress _to, BigInt _tokenId,
-      {required _i1.Credentials credentials,
-      _i1.Transaction? transaction}) async {
+    _i1.EthereumAddress _from,
+    _i1.EthereumAddress _to,
+    BigInt _tokenId, {
+    required _i1.Credentials credentials,
+    _i1.Transaction? transaction,
+  }) async {
     final function = self.abi.functions[0];
     assert(checkSignature(function, '42842e0e'));
-    final params = [_from, _to, _tokenId];
-    return write(credentials, transaction, function, params);
+    final params = [
+      _from,
+      _to,
+      _tokenId,
+    ];
+    return write(
+      credentials,
+      transaction,
+      function,
+      params,
+    );
   }
 
   /// The optional [transaction] parameter can be used to override parameters
   /// like the gas price, nonce and max gas. The `data` and `to` fields will be
   /// set by the contract.
-  Future<String> safeTransferFrom$2(_i1.EthereumAddress _from,
-      _i1.EthereumAddress _to, BigInt _tokenId, _i2.Uint8List data,
-      {required _i1.Credentials credentials,
-      _i1.Transaction? transaction}) async {
+  Future<String> safeTransferFrom$2(
+    _i1.EthereumAddress _from,
+    _i1.EthereumAddress _to,
+    BigInt _tokenId,
+    _i2.Uint8List data, {
+    required _i1.Credentials credentials,
+    _i1.Transaction? transaction,
+  }) async {
     final function = self.abi.functions[1];
     assert(checkSignature(function, 'b88d4fde'));
-    final params = [_from, _to, _tokenId, data];
-    return write(credentials, transaction, function, params);
+    final params = [
+      _from,
+      _to,
+      _tokenId,
+      data,
+    ];
+    return write(
+      credentials,
+      transaction,
+      function,
+      params,
+    );
   }
 }
 ''',
