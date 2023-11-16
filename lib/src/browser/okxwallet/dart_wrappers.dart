ea5f3608ba9945dd69be012325d7c6b104cb7b8a
redDwarf03 <sylvain@archethic.net>
2023-11-16 17:50:27 +0100

    Handle error

--- lib/src/browser/okxwallet/dart_wrappers.dart
+++ lib/src/browser/okxwallet/dart_wrappers.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:js/js_util.dart';
+import 'package:webthree/src/core/exception_utils_io.dart';
 
 import '../../../credentials.dart';
 import '../../../json_rpc.dart';
@@ -43,7 +44,9 @@ extension DartOkxWallet on OkxWallet {
     final args = params == null
         ? RequestArguments(method: method)
         : RequestArguments(method: method, params: params);
-    return promiseToFuture(request(args));
+    return promiseToFuture(request(args)).onError((error, stackTrace) {
+      ExceptionUtils.analyzeException(error!);
+    });
   }
 
   /// Asks the user to select an account and give your application access to it.
