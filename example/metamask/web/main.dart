5044f59190cc721b784c48255aed61e2ed3d2ddd
redDwarf03 <sylvain@archethic.net>
2023-11-22 14:13:22 +0100

    🐛 Fix import

--- example/metamask/web/main.dart
+++ example/metamask/web/main.dart
@@ -2,8 +2,23 @@ import 'dart:convert';
 import 'dart:html';
 import 'dart:typed_data';
 
-import 'package:webthree/browser.dart';
+//conditionally import dependencies in order to support web and other platform builds from a single codebase
+import 'package:js/js.dart'
+    if (dart.library.io) 'package:webthree/lib/src/browser/js-stub.dart'
+    if (dart.library.js) 'package:js/js.dart';
+import 'package:webthree/browser.dart'
+    if (dart.library.io) 'package:webthree/lib/src/browser/dart_wrappers_stub.dart'
+    if (dart.library.js) 'package:webthree/browser.dart';
 import 'package:webthree/webthree.dart';
+
+@JS()
+@anonymous
+class JSrawRequestParams {
+  external String get chainId;
+
+  // Must have an unnamed factory constructor with named arguments.
+  external factory JSrawRequestParams({String chainId});
+}
 
 Future<void> main() async {
   await metamask();
@@ -22,9 +37,15 @@ Future<void> metamask() async {
   print('Using ${credentials.address}');
   print('Client is listening: ${await client.isListeningForNetwork()}');
 
-  final message = Uint8List.fromList(utf8.encode('Hello from web3dart'));
+  final message = Uint8List.fromList(utf8.encode('Hello from webthree'));
   final signature = await credentials.signPersonalMessage(message);
   print('Signature: ${base64.encode(signature)}');
+
+  await eth.rawRequest('wallet_switchEthereumChain',
+      params: [JSrawRequestParams(chainId: '0x507')]);
+  final String chainIDHex = await eth.rawRequest('eth_chainId') as String;
+  final chainID = int.parse(chainIDHex);
+  print('chainID: $chainID');
 }
 
 Future<void> binanceChainWallet() async {
@@ -40,9 +61,11 @@ Future<void> binanceChainWallet() async {
   print('Using ${credentials.address}');
   print('Client is listening: ${await client.isListeningForNetwork()}');
 
-  final message = Uint8List.fromList(utf8.encode('Hello from web3dart'));
+  final message = Uint8List.fromList(utf8.encode('Hello from webthree'));
   final signature = await credentials.signPersonalMessage(message);
   print('Signature: ${base64.encode(signature)}');
+
+  // wallet_switchEthereumChain not available with this wallet
 }
 
 Future<void> okxWallet() async {
@@ -58,7 +81,9 @@ Future<void> okxWallet() async {
   print('Using ${credentials.address}');
   print('Client is listening: ${await client.isListeningForNetwork()}');
 
-  final message = Uint8List.fromList(utf8.encode('Hello from web3dart'));
+  final message = Uint8List.fromList(utf8.encode('Hello from webthree'));
   final signature = await credentials.signPersonalMessage(message);
   print('Signature: ${base64.encode(signature)}');
+
+  // wallet_switchEthereumChain not available with this wallet
 }
